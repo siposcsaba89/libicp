9383e9ca19458f7af991e7900fae012cfb309c6c
Csaba Sipos <csaba.sipos@faulhornlabs.com>
2021-09-22 17:41:05 +0200

    cmake targets and install

--- src/matrix.h
+++ src/matrix.h
@@ -27,23 +27,12 @@ Street, Fifth Floor, Boston, MA 02110-1301, USA
 #include <iostream>
 #include <vector>
 
-#ifndef _MSC_VER
-  #include <stdint.h>
-#else
-  typedef signed __int8     int8_t;
-  typedef signed __int16    int16_t;
-  typedef signed __int32    int32_t;
-  typedef signed __int64    int64_t;
-  typedef unsigned __int8   uint8_t;
-  typedef unsigned __int16  uint16_t;
-  typedef unsigned __int32  uint32_t;
-  typedef unsigned __int64  uint64_t;
-#endif
+#include <inttypes.h>
 
 #define endll endl << endl // double end line definition
 
-typedef double FLOAT;      // double precision
-//typedef float  FLOAT;    // single precision
+typedef double LIBICP_FLOAT;      // double precision
+//typedef float  LIBICP_FLOAT;    // single precision
 
 class Matrix {
 
@@ -52,7 +41,7 @@ public:
   // constructor / deconstructor
   Matrix ();                                                  // init empty 0x0 matrix
   Matrix (const int32_t m,const int32_t n);                   // init empty mxn matrix
-  Matrix (const int32_t m,const int32_t n,const FLOAT* val_); // init mxn matrix with values from array 'val'
+  Matrix (const int32_t m,const int32_t n,const LIBICP_FLOAT* val_); // init mxn matrix with values from array 'val'
   Matrix (const Matrix &M);                                   // creates deepcopy of M
   ~Matrix ();
 
@@ -60,17 +49,17 @@ public:
   Matrix& operator= (const Matrix &M);
 
   // copies submatrix of M into array 'val', default values copy whole row/column/matrix
-  void getData(FLOAT* val_,int32_t i1=0,int32_t j1=0,int32_t i2=-1,int32_t j2=-1);
+  void getData(LIBICP_FLOAT* val_,int32_t i1=0,int32_t j1=0,int32_t i2=-1,int32_t j2=-1);
 
   // set or get submatrices of current matrix
   Matrix getMat(int32_t i1,int32_t j1,int32_t i2=-1,int32_t j2=-1);
   void   setMat(const Matrix &M,const int32_t i,const int32_t j);
 
   // set sub-matrix to scalar (default 0), -1 as end replaces whole row/column/matrix
-  void setVal(FLOAT s,int32_t i1=0,int32_t j1=0,int32_t i2=-1,int32_t j2=-1);
+  void setVal(LIBICP_FLOAT s,int32_t i1=0,int32_t j1=0,int32_t i2=-1,int32_t j2=-1);
 
   // set (part of) diagonal to scalar, -1 as end replaces whole diagonal
-  void setDiag(FLOAT s,int32_t i1=0,int32_t i2=-1);
+  void setDiag(LIBICP_FLOAT s,int32_t i1=0,int32_t i2=-1);
 
   // clear matrix
   void zero();
@@ -92,43 +81,43 @@ public:
   static Matrix reshape(const Matrix &M,int32_t m,int32_t n);
 
   // create 3x3 rotation matrices (convention: http://en.wikipedia.org/wiki/Rotation_matrix)
-  static Matrix rotMatX(const FLOAT &angle);
-  static Matrix rotMatY(const FLOAT &angle);
-  static Matrix rotMatZ(const FLOAT &angle);
+  static Matrix rotMatX(const LIBICP_FLOAT &angle);
+  static Matrix rotMatY(const LIBICP_FLOAT &angle);
+  static Matrix rotMatZ(const LIBICP_FLOAT &angle);
 
   // simple arithmetic operations
   Matrix  operator+ (const Matrix &M); // add matrix
   Matrix  operator- (const Matrix &M); // subtract matrix
   Matrix  operator* (const Matrix &M); // multiply with matrix
-  Matrix  operator* (const FLOAT &s);  // multiply with scalar
+  Matrix  operator* (const LIBICP_FLOAT &s);  // multiply with scalar
   Matrix  operator/ (const Matrix &M); // divide elementwise by matrix (or vector)
-  Matrix  operator/ (const FLOAT &s);  // divide by scalar
+  Matrix  operator/ (const LIBICP_FLOAT &s);  // divide by scalar
   Matrix  operator- ();                // negative matrix
   Matrix  operator~ ();                // transpose
-  FLOAT   l2norm ();                   // euclidean norm (vectors) / frobenius norm (matrices)
-  FLOAT   mean ();                     // mean of all elements in matrix
+  LIBICP_FLOAT   l2norm ();                   // euclidean norm (vectors) / frobenius norm (matrices)
+  LIBICP_FLOAT   mean ();                     // mean of all elements in matrix
 
   // complex arithmetic operations
   static Matrix cross (const Matrix &a, const Matrix &b);    // cross product of two vectors
   static Matrix inv (const Matrix &M);                       // invert matrix M
   bool   inv ();                                             // invert this matrix
-  FLOAT  det ();                                             // returns determinant of matrix
-  bool   solve (const Matrix &M,FLOAT eps=1e-20);            // solve linear system M*x=B, replaces *this and M
-  bool   lu(int32_t *idx, FLOAT &d, FLOAT eps=1e-20);        // replace *this by lower upper decomposition
+  LIBICP_FLOAT  det ();                                             // returns determinant of matrix
+  bool   solve (const Matrix &M,LIBICP_FLOAT eps=1e-20);            // solve linear system M*x=B, replaces *this and M
+  bool   lu(int32_t *idx, LIBICP_FLOAT &d, LIBICP_FLOAT eps=1e-20);        // replace *this by lower upper decomposition
   void   svd(Matrix &U,Matrix &W,Matrix &V);                 // singular value decomposition *this = U*diag(W)*V^T
 
   // print matrix to stream
   friend std::ostream& operator<< (std::ostream& out,const Matrix& M);
 
   // direct data access
-  FLOAT   **val;
+  LIBICP_FLOAT   **val;
   int32_t   m,n;
 
 private:
 
   void allocateMemory (const int32_t m_,const int32_t n_);
   void releaseMemory ();
-  inline FLOAT pythag(FLOAT a,FLOAT b);
+  inline LIBICP_FLOAT pythag(LIBICP_FLOAT a,LIBICP_FLOAT b);
 
 };
 
